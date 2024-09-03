2b632fab58b9879894ec7020fab6aae7dc6e00fc
Heithor Barbosa <heithor.barbosa321@gmail.com>
2024-09-03 10:03:31 -0300

    EX-3, IMC foi

--- dia-3/index.js
+++ dia-3/index.js
@@ -1,13 +1,15 @@
 let name = prompt("qual é o seu nome ?");
 let date = parseInt(prompt("quantos anos você tem ?"));
 let weight = parseInt(prompt("quanto você pesa ?"));
-let height = parseInt(prompt("qual é a sua altura ?"));
+let height = parseFloat(
+  prompt("qual é a sua altura ? (use ponto no lugar da vírgula)")
+);
 let IMC = 0;
 
 let bornDate = 2023 - date;
 IMC = weight / (height * height);
 
-console.log(
+document.write(
   "Olá ",
   name,
   ", você nasceu em ",
