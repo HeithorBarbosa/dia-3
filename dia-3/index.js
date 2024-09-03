2c970770d375d63fc0b33291234dc309e6884b34
Heithor Barbosa <heithor.barbosa321@gmail.com>
2024-09-02 22:56:57 -0300

    ex dia 3

--- dia-3/index.js
+++ dia-3/index.js
@@ -1,3 +1,19 @@
-let typenumber = 8;
-let typetext = "hello world";
-let typebool = true;
+let name = prompt("qual é seu nome ?");
+let date = prompt("quantos anos vôce tem ?");
+let peso = prompt("qual é o seu peso ?");
+let altura = prompt("qual é a sua altura ?");
+
+let dataDeNascimento = 2023 - date;
+let IMC = peso / (altura * altura);
+document.write(
+  "Olá ",
+  name,
+  ", você nasceu em ",
+  dataDeNascimento,
+  ", possui o peso de ",
+  peso,
+  ", tem a altura de ",
+  altura,
+  " e o IMC equivale a: ",
+  IMC
+);
